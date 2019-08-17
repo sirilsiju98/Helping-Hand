3023ab463216eea9e4b98db43c194a89898dd5ee
sirilsiju98 <sirilsiju98@gmail.com>
2019-08-17 22:44:09 +0530

    Modified

---
+++ app/src/main/java/helping/hand/CreateAC.java
@@ -0,0 +1,14 @@
+package helping.hand;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateAC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_ac);
+    }
+}
