8a691987a148af90ab53a868338cd4c51b270d25
sirilsiju98 <sirilsiju98@gmail.com>
2019-08-23 18:28:48 +0000

    Modified

--- app/src/main/java/helping/hand/Items.java
+++ app/src/main/java/helping/hand/Items.java
@@ -8,7 +8,7 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
-import android.app.Activity;
+
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
