3061df1a12848968b9cc37ba24f5b6ccaba05e67
sirilsiju98 <sirilsiju98@gmail.com>
2019-08-23 11:54:53 +0000

    Modified

--- app/src/main/java/helping/hand/Items.java
+++ app/src/main/java/helping/hand/Items.java
@@ -7,6 +7,8 @@ import android.app.ProgressDialog;
 import android.os.Bundle;
 
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.parse.FindCallback;
@@ -14,6 +16,8 @@ import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -25,20 +29,37 @@ public class Items extends AppCompatActivity {
         setContentView(R.layout.activity_items);
         setTitle("Available items");
 
-        ParseQuery<ParseObject> query =ParseQuery.getQuery("Items_Available");
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Items_Available");
         query.findInBackground(new FindCallback<ParseObject>() {
             @Override
             public void done(List<ParseObject> objects, ParseException e) {
 
-                    if(objects.toString().equals("[]"))
+                if (e == null) {
+                    if (objects.size() == 0)
                         Toast.makeText(Items.this, "Sorry no items are Available", Toast.LENGTH_SHORT).show();
 
+                    else
+
+                    {
+                        ListView listView = (ListView)findViewById(R.id.avail);
+                        final ArrayList<String> list = new ArrayList<String>();
+                        for (ParseObject object : objects){
+                            String ele=object.get("user")+"\nRice(in Kg): "+object.get("Rice")+"\nWheat(in kg): "+
+                                    object.get("Wheat")+"\nDress Material: "+object.get("Dress_Material")+"\nToys:"+object.get("Toys")+
+                                    "\nLocation: "+object.get("Location")+"\nContact :"+object.get("contact");
+                            list.add(ele);
+
+                        }
+                        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(Items.this,android.R.layout.simple_list_item_1,list);
+                        listView.setAdapter(arrayAdapter);
+                    }
+                }
             }
         });
-
-
     }
-
-
-
 }
+
+
+
+
+
